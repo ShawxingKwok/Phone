ac96541385859b2a7cf58b7e8d0caa3183782381
WilliamKwokX <1425183716@qq.com>
2023-10-09 18:22:49 +0800

    +

--- testapi/build.gradle.kts
+++ testapi/build.gradle.kts
@@ -31,6 +31,6 @@ ksp{
     arg("ksp-util.debug", "")
     arg("phone.server-package-path", "/Users/william/IdeaProjects/library/Phone/testcaller/src/commonMain/kotlin")
     arg("phone.client-package-path", "/Users/william/IdeaProjects/library/Phone/testcaller/src/commonMain/kotlin")
-    arg("phone.server-package-name", "pers.shawxingkwok.test")
-    arg("phone.client-package-name", "pers.shawxingkwok.test")
+    arg("phone.server-package-name", "pers.shawxingkwok.test.server")
+    arg("phone.client-package-name", "pers.shawxingkwok.test.client")
 }
