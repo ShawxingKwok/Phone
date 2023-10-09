5f5a3b8a4c7da17e42205d90dfd720877c7b3f23
WilliamKwokX <1425183716@qq.com>
2023-10-09 19:02:13 +0800

    +

--- compiler/src/jvmMain/kotlin/pers/shawxingkwok/phone/getNeededFunctions.kt
+++ compiler/src/jvmMain/kotlin/pers/shawxingkwok/phone/getNeededFunctions.kt
@@ -1,6 +1,5 @@
 package pers.shawxingkwok.phone
 
-import com.google.devtools.ksp.isOpen
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.google.devtools.ksp.symbol.Modifier
