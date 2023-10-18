99e379e5b3152d88350468788f2effc6c7244f4c
WilliamKwokX <1425183716@qq.com>
2023-10-18 17:13:31 +0800

    +

--- compiler/src/main/kotlin/pers/shawxingkwok/phone/client/getWebSocketBody.kt
+++ compiler/src/main/kotlin/pers/shawxingkwok/phone/client/getWebSocketBody.kt
@@ -18,44 +18,29 @@ internal fun KSClassDeclaration.getWebSocketBody(webSocket: Phone.WebSocket): St
         isTopLevelAndExtensional = false,
     )
 
-    val functionsText = getNeededFunctions()
-        .joinToString("\n\n"){
-            it.getWebSocketBody(this, webSocket)
-        }
-
     return  """
         inner class $phoneName(
             private val act: suspend ($webSocketSessionKSClassText) -> Unit
         )
             : ${qualifiedName()}
         {
-            private val basicUrl = "${"$"}mBasicUrl/${phoneName}" 
+            ${getNeededFunctions().joinToString("\n\n"){ ksfun ->
+                """
+                ${ksfun.getClientFunctionHeader()} {
+                    maySecureWebSocket${insertIf(webSocket.isRaw){ "Raw" }}(
+                    path = "${'$'}mBasicUrl/$phoneName/${ksfun.simpleName()}${ksfun.mayPolymorphicId}",
+                    request = {
+                        ${insertIf(webSocket.subProtocol.any()){ "header(HttpHeaders.SecWebSocketProtocol, \"${webSocket.subProtocol}\")" }}
             
-            $functionsText
+                        ${insertIf(ksfun.parameters.any()){ ksfun.getJsonParametersBody(this) }}
+                    },
+                ){
+                    checkNoBadRequest(call.response)
+                    act(this)
+                }
+                }
+                """.trim()
+            }}
         }
         """.trim()
 }
-
-context (CodeFormatter)
-private fun KSFunctionDeclaration.getWebSocketBody(
-    ksclass: KSClassDeclaration,
-    webSocket: Phone.WebSocket,
-) =
-    buildString {
-        val path = "${'$'}basicUrl/${simpleName()}$mayPolymorphicId"
-
-        append("""
-        ${getClientFunctionHeader()} {
-            maySecureWebSocket${insertIf(webSocket.isRaw){ "Raw" }}(
-                path = "$path",
-                request = {
-                    ${insertIf(webSocket.subProtocol.any()){ "header(HttpHeaders.SecWebSocketProtocol, \"${webSocket.subProtocol}\")" }}
-                    ${insertIf(parameters.any()){ getJsonParametersBody(ksclass) }} 
-                },
-            ){
-                checkNoBadRequest(call.response)
-                act(this)
-            }
-        }
-        """.trim())
-    }
