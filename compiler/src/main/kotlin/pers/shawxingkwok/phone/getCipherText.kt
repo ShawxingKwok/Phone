912e31d8ee8f664b002b155b7bbf0f3db0bf6b7d
WilliamKwokX <1425183716@qq.com>
2023-10-19 19:17:49 +0800

    +

--- compiler/src/main/kotlin/pers/shawxingkwok/phone/getCipherText.kt
+++ compiler/src/main/kotlin/pers/shawxingkwok/phone/getCipherText.kt
@@ -3,8 +3,10 @@ package pers.shawxingkwok.phone
 import com.google.devtools.ksp.isAnnotationPresent
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
+import com.google.devtools.ksp.symbol.KSNode
 import com.google.devtools.ksp.symbol.KSValueParameter
 import pers.shawxingkwok.ksputil.CodeFormatter
+import pers.shawxingkwok.ksputil.Log
 
 context (CodeFormatter)
 internal fun KSFunctionDeclaration.getCipherTextForReturn(srcKSClass: KSClassDeclaration): String? {
@@ -14,7 +16,7 @@ internal fun KSFunctionDeclaration.getCipherTextForReturn(srcKSClass: KSClassDec
         // parent function may be from a super class
         || srcKSClass.isAnnotationPresent(Phone.Crypto::class)
 
-    return getCipherText(isCrypto)
+    return getCipherText(this, isCrypto)
 }
 
 context (CodeFormatter)
@@ -25,12 +27,12 @@ internal fun KSValueParameter.getCipherText(srcKSClass: KSClassDeclaration): Str
         || (parent as KSFunctionDeclaration).isAnnotationPresent(Phone.Crypto::class)
         || srcKSClass.isAnnotationPresent(Phone.Crypto::class)
 
-    return getCipherText(isCrypto)
+    return getCipherText(this, isCrypto)
 }
 
 context (CodeFormatter)
-private fun getCipherText(isCrypto: Boolean): String? =
+private fun getCipherText(symbol: KSNode, isCrypto: Boolean): String? =
     if (isCrypto)
-        MyProcessor.cipherKSObj?.text ?: error(TODO("link"))
+        MyProcessor.cipherKSObj?.text ?: Log.e(symbol, "$symbol is crypto without cipher.")
     else
         null
